983ff7df540239e1835b3033c59d70df49dc79dd
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-02-28 16:45:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,33 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+############################
+# Route
+############################
+resource "google_compute_route" "default_route" {
+  name             = "${var.environment}-route"
+  network          = google_compute_network.vpc.name
+  dest_range       = "0.0.0.0/0"
+  next_hop_gateway = "default-internet-fateway"
+}
+
+############################
+# VM Instance
+############################
+resource "google_compute_instance" "vm" {
+  name         = "${var.environment}-vm"
+  machine_type = var.env_config[var.environment].machine_type
+  zone         = "${var.region}-a"
+
+  boot_disk {
+    initialize_params {
+      image  = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_network.subnet.id
+    access_config {}
+  }
+}
+
+
