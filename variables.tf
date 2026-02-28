6c35bc6e20fae0c7168236c0957d6a197ef0497f
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-02-28 17:38:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@ variable "project_id" {
 variable "region" {
   type        = string
   default     = "us-central1"
+  description = "GCP Region"
 }
 
 variable "environment" {
@@ -13,29 +14,33 @@ variable "environment" {
   description = "Environment (dev, test, prod)"
 
   validation {
-    condition      = contains(["dev", "test', "prod"], var.Environment),
-    error_message  = "Environment must be dev, test, or prod."
+    condition     = contains(["dev", "test", "prod"], var.environment)
+    error_message = "Environment must be dev, test, or prod."
   }
 }
 
 variable "env_config" {
   description = "Environment specific configurations"
+
   type = map(object({
     machine_type = string
     bucket_name  = string
   }))
+
   default = {
-     dev = {
-        machine_type = "e2-medium"
-        bucket_name  = "dev-bucket-terrfaorm"
-     }
-     test = {
-       meachine_type = "e2-standard-2"
-       bucket_name   = "test-bucket-terraform"
-     }
-     prod {
-       machine_type = "e2-standard-4"
-       bucket_name  = "prod-bucket-terraform"
-     }
+    dev = {
+      machine_type = "e2-medium"
+      bucket_name  = "dev-bucket-terraform"
+    }
+
+    test = {
+      machine_type = "e2-standard-2"
+      bucket_name  = "test-bucket-terraform"
+    }
+
+    prod = {
+      machine_type = "e2-standard-4"
+      bucket_name  = "prod-bucket-terraform"
+    }
   }
 }
