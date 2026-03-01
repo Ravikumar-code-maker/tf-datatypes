b75a8ee3f97ce9c0681f0ef95a778afbbeb3fd64
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-03-01 13:41:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,8 +75,7 @@ resource "google_api_gateway_api_config" "api_config" {
 
 resource "google_api_gateway_gateway" "gateway" {
   provider   = google-beta
-  gateway_id = "${var.environment}-gateway"
-  name       = "${var.environment}-gateway"
+  gateway_id = "${var.environment}-gateway"  # This is what you control
   api_config = google_api_gateway_api_config.api_config.id
   region     = var.region
 }
